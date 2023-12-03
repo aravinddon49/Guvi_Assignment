eafa6cf907aba56d42a86d663c88b776e2821191
aravinddon49 <149284029+aravinddon49@users.noreply.github.com>
2023-12-03 16:26:00 +0530

    Delete Task5_Question1.py

--- Task5_Question1.py
+++
@@ -1,7 +0,0 @@
-subject = "Guvi Geeks Network Private Limited"
-vowels = "aeiou"
-
-no_of_vowels = sum(subject.count(vowel) for vowel in vowels)
-print(f"Total number of vowels:",no_of_vowels)
-count_of_vowels=(vowels.count(len(subject)))
-print(f"Count of vowels:", count_of_vowels)
